ebce0d72526258e5ac5f0120d1f87215b5316399
tapan79 <111303759+tapan79@users.noreply.github.com>
2026-07-18 15:44:36 +0530

    Fix: Add events for player participation and winner selection
    - Added PlayerJoined event
    - Added WinnerSelected event
    - Emitted events on player participation and winner selection

---
+++ projects/Lottery/Lottery_Fixed.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Lottery
+{
+    // owner or manager of contract
+    address public manager;
+
+    // address of players - create dynamic array whose name is players
+    address payable[] public players;
+
+    // address of winner
+    address payable public winner;
+
+    constructor()
+    {
+        manager = msg.sender; // msg.sender is deployer whose address is stored in manager
+    }
+
+    event PlayerJoined(address indexed player); // create event for blockchain log - participants added
+    function participate() public payable
+    {
+        require(msg.value == 1 ether, "Please pay only one ether"); // store the ether in contract address
+        players.push(payable(msg.sender)); // store the address in players dynamic size array
+
+        emit PlayerJoined(msg.sender);
+    }
+
+    // to get balance of this contract or deployer
+    function getbalance() public view returns(uint)
+    {
+        require(manager == msg.sender, "only manager allow this function to check the balance of contract");
+        return address(this).balance; // check the total balance of contract, the function called by only the manager.
+    }
+
+    function random() internal view returns(uint) // here, internal visibilty used means contract call itself this function
+    {
+        return uint(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, players.length)));
+    }
+
+    event WinnerSelected(address indexed winner, uint256 prize); // create event for selecting the winner and amount transfer to the winner
+
+    function pickwinner() public
+    {
+        require(manager == msg.sender, "You are not manager");
+        require(players.length >= 3, "Players are less than three");
+
+        uint index = random() % players.length;
+        winner = players[index];
+        uint amount = address(this).balance;
+        (bool success, ) = winner.call{value: amount}("");
+        require(success, "Transfer failed");
+
+        emit WinnerSelected(winner, amount);
+
+        players = new address payable[](0); // reset the players address
+    }
+}
